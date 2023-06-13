90c63288aa161c80dc6e9d31e1e532fa84930ba5
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-06-13 15:00:53 +0200

    Lesson about equals and binarySearch Arrays methods.

--- src/main/java/dev/lpa/Main.java
+++ src/main/java/dev/lpa/Main.java
@@ -31,6 +31,22 @@ public class Main {
 
         int[] largerArray = Arrays.copyOf(thirdArray, 15);
         System.out.println(Arrays.toString(largerArray));
+
+        String[] sArray = {"Able", "Jane", "Mark", "Ralph", "David"};
+        Arrays.sort(sArray);
+        System.out.println(Arrays.toString(sArray));
+        if (Arrays.binarySearch(sArray, "Mark") >= 0) {
+            System.out.println("Found Mark in the List");
+        }
+
+        int[] s1 = {1, 2, 3, 4, 5};
+        int[] s2 = {5, 2, 3, 4, 5};
+
+        if (Arrays.equals(s1, s2)) {
+            System.out.println("Arrays are equal");
+        } else {
+            System.out.println("Arrays are not equal");
+        }
     }
 
     private static int[] getRandomArray(int len) {
